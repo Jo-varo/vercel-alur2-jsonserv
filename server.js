141073df4955ade54512692b7dee8fa7ef46283d
jovaro <jorgevargas0240@gmail.com>
2024-02-13 18:28:17 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import jsonServer from 'json-server'
+import fs from 'fs'
+import path from 'path'
+
+const server = jsonServer.create();
+
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+export default server;
